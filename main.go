2e3f875c8171590545d4da42486da554a04016ae
Patrick D'appollonio <930925+patrickdappollonio@users.noreply.github.com>
2023-02-26 23:35:58 -0500

    Update with changes.

--- main.go
+++ main.go
@@ -1,46 +1,33 @@
 package main
 
 import (
-	"encoding/json"
+	_ "embed"
 	"log"
 	"net/http"
+	"text/template"
 )
+
+// embed the public templating code
+//
+//go:embed templates/index.html
+var code string
+
+var compiledTemplate = template.Must(template.New("index").Parse(code))
 
 func main() {
 	mux := http.NewServeMux()
 
 	// This endpoint will show any content in the "public" directory
 	// and will also serve the index.html file as the homepage
-	mux.HandleFunc("/", http.FileServer(http.Dir("public")).ServeHTTP)
-
-	// This is an imaginary endpoint to handle subroutes under /api
-	mux.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("This is my API"))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		compiledTemplate.Execute(w, nil)
 	})
 
-	mux.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
-		var content struct {
-			Name string `json:"name"`
-		}
-
-		// I decode the request body into the content variable
-		// and if it fails, I return a 400 Bad Request
-		if err := json.NewDecoder(r.Body).Decode(&content); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		// Generate a custom response
-		resp := map[string]interface{}{
-			"message": "Hello " + content.Name,
-		}
-
-		// I encode the content variable into the response body
-		// and if it fails, I return a 500 Internal Server Error
-		if err := json.NewEncoder(w).Encode(resp); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	mux.HandleFunc("/form/hello", func(w http.ResponseWriter, r *http.Request) {
+		name := r.FormValue("name")
+		compiledTemplate.Execute(w, map[string]interface{}{
+			"Name": name,
+		})
 	})
 
 	// I create a server, no framework, just pure Go
